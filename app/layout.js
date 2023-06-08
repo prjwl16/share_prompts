5a8f54009f56a3d60df142e75fdd78d28553ebc1
prjwl16 <prajumore76@gmail.com>
2023-06-08 15:07:27 +0530

    trying

--- app/layout.js
+++ app/layout.js
@@ -9,17 +9,7 @@ export const metaData = {
 const RootLayout = ({ children }) => {
   return (
     <html lang="en">
-      <body>
-        <Provider>
-          <div className="main">
-            <div className="gradient" />
-          </div>
-          <main className="app">
-            <Nav />
-            {children}
-          </main>
-        </Provider>
-      </body>
+      <body></body>
     </html>
   );
 };
