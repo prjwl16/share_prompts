7f3f9603c06a7fd393e2107a9c9f2f56ba652c5a
prjwl16 <prajumore76@gmail.com>
2023-06-08 15:20:27 +0530

    try 3

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,5 @@
 import Nav from "@components/Nav";
-// import Provider from "@components/Provider";
+import Provider from "@components/Provider";
 import "@styles/globals.css";
 
 export const metaData = {
@@ -9,7 +9,17 @@ export const metaData = {
 const RootLayout = ({ children }) => {
   return (
     <html lang="en">
-      <body></body>
+      <body>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
+      </body>
     </html>
   );
 };
